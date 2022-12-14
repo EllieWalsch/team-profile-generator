c59768b78ce68ccc55556e099ac83224f76d3a12
EllieWalsch <elliewalsch4@gmail.com>
2022-12-14 16:14:14 -0600

    Generate card HTML for each class

--- src/generateHTML.js
+++ src/generateHTML.js
@@ -1,1 +1,55 @@
-// generates HTML from input
+// this will generate HTML for each class
+
+const generateManagerHTML = (managerArr) => `
+  <div class="card m-3" style="width: 18rem;">
+    <div class="card-body">
+      <h3 class="card-title bg-primary p-3 text-white text-center">${managerArr.getName()}</h3>
+      <ul class="list-group list-group-flush">
+        <li class="list-group-item">Email: ${managerArr.getEmail()}</li>
+        <li class="list-group-item">ID: ${managerArr.getId()}</li>
+        <li class="list-group-item">Office Number: ${managerArr.getOfficeNumber()}</li>
+      </ul>
+    </div>
+  </div>`;
+
+const generateInternHTML = (internArr) => {
+  const internHTML = [];
+  for (let i = 0; i < internArr.length; i++) {
+    internHTML.push(
+      `<div class="card m-3" style="width: 18rem;">
+        <div class="card-body">
+          <h3 class="card-title bg-primary p-3 text-white text-center">${internArr.getName()}</h3>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Email: ${internArr[i].getEmail()}</li>
+            <li class="list-group-item">ID: ${internArr[i].getId()}</li>
+            <li class="list-group-item">School: ${internArr[i].getSchool()}</li>
+          </ul>
+        </div>
+      </div>`
+    );
+  }
+
+  return internHTML;
+};
+
+const generateEngineerHTML = (engineerArr) => {
+  const engineerHTML = [];
+  for (let i = 0; i < engineerArr.length; i++) {
+    engineerHTML.push(
+      `<div class="card m-3" style="width: 18rem;">
+        <div class="card-body">
+          <h3 class="card-title bg-primary p-3 text-white text-center">${engineerArr.getName()}</h3>
+          <ul class="list-group list-group-flush">
+            <li class="list-group-item">Email: ${engineerArr[i].getEmail()}</li>
+            <li class="list-group-item">ID: ${engineerArr[i].getId()}</li>
+            <li class="list-group-item">Github: ${engineerArr[
+              i
+            ].getGithub()}</li>
+          </ul>
+        </div>
+      </div>`
+    );
+  }
+
+  return engineerHTML;
+};
