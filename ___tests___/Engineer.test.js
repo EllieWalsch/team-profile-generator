905c4866ddbb0c7212601d93e74ad482655823d8
EllieWalsch <elliewalsch4@gmail.com>
2022-12-20 15:03:51 -0600

    Add engineer tests

---
+++ ___tests___/Engineer.test.js
@@ -0,0 +1,14 @@
+import { expect, it } from "vitest";
+import { Engineer } from "../lib/Engineer";
+
+it("Should create a new Github username", () => {
+  const github = "username";
+  const engineer = new Engineer(github);
+  expect(engineer.getGithub()).toEqual(github);
+});
+
+it("Should overwrite role to be Engineer", () => {
+  const role = "Engineer";
+  const engineer = new Engineer(role);
+  expect(engineer.getRole()).toEqual(role);
+});
